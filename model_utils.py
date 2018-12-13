10b47223827b2cc6cadcd99232c97eda77f1303b
Naresh <NareshPS@github.com>
2018-12-13 10:59:53 -0600

    Refactor fit_generator to a method in model_utils.py

--- model_utils.py
+++ model_utils.py
@@ -16,23 +16,23 @@ from random import shuffle
 from utils import load_images_batch
 from common import constants
 
-def get_image_labels():
+def get_input_labels():
     """Creates a mapping of image file names to  labels.
 
     Returns:
         {string : string} -- A dictionary with file name mapped to its label.
     """
     #Load labels
-    image_labels = {}
+    input_labels = {}
 
     with open(constants.RAW_DATASET_MAPPINGS["labels"], 'r') as handle:
         label_reader = csv.reader(handle)
         next(label_reader, None)
         
         for row in label_reader:
-            image_labels[row[0]] = row[1]
+            input_labels[row[0]] = row[1]
 
-        return image_labels
+        return input_labels
 
 def get_label_ids():
     """Assigns a unique index to a label.
@@ -40,8 +40,8 @@ def get_label_ids():
     Returns:
         {string:int} -- A dictionary that maps a label to an index.
     """
-    image_labels = get_image_labels()
-    label_ids = {label : idx for idx, label in enumerate(set(image_labels.values()))}
+    input_labels = get_input_labels()
+    label_ids = {label : idx for idx, label in enumerate(set(input_labels.values()))}
 
     return label_ids
 
@@ -51,17 +51,17 @@ def get_all_labels():
     Returns:
         [string] -- A list of training labels 
     """
-    image_labels = get_image_labels()
-    return set(image_labels.values())
+    input_labels = get_input_labels()
+    return set(input_labels.values())
 
-def load_training_batch(requestor, source_loc, img_files, batch_size, image_labels, label_ids):
+def load_training_batch(requestor, source_loc, img_files, batch_size, input_labels, label_ids):
     """It loads training data in batches.
     
     Arguments:
         source_loc {string} -- Location of preprocessed image files.
         img_files {[type]} -- List of image file names.
         batch_size {[type]} -- Number of images to load in a batch.
-        image_labels {{string:string}} -- A mapping of image file name to their labels.
+        input_labels {{string:string}} -- A mapping of image file name to their labels.
         label_ids {{string:int}} -- A mapping of label to a unique index.
     """
 
@@ -71,12 +71,12 @@ def load_training_batch(requestor, source_loc, img_files, batch_size, image_labe
         x = np.asarray(imgs)/255
 
         #Assign label indices.
-        y = [label_ids[image_labels[img_file]] for img_file in batch_img_files]
+        y = [label_ids[input_labels[img_file]] for img_file in batch_img_files]
         y = to_categorical(y, num_classes = num_classes)
 
         yield [x], y
 
-def model_fit_data_feeder(requestor, source_loc, img_files, batch_size, image_labels, label_ids):
+def _model_fit_data_feeder(requestor, source_loc, img_files, batch_size, image_labels, label_ids):
     """It is used in fit_generator() to supply the training and the validation data.
     
     Arguments:
@@ -90,3 +90,33 @@ def model_fit_data_feeder(requestor, source_loc, img_files, batch_size, image_la
         shuffle(img_files)
         for x, y in load_training_batch(requestor, source_loc, img_files, batch_size, image_labels, label_ids):
             yield x, y
+
+def model_fit(model, source_loc, input_set, input_labels, label_ids, batch_size, n_epochs, validation_split):
+    """Splits the data into two sets. Training set is used for training the model. Validation set validates it.
+    Training is conducted in batches.
+    
+    Arguments:
+        model {keras model object} -- Keras model objects.
+        source_loc {string} -- The location of input set.
+        input_set {[string]} -- A list of items in the input set.
+        input_labels {[string]} -- A mapping from input item name to its label.
+        label_ids {[int]} -- A mapping from input label to its id.
+        batch_size {int} -- The batch size.
+        n_epochs {int} -- The number of epochs to train the model.
+        validation_split {float} -- A float between [0, 1] indicating the split of train and validation sets.
+    """
+
+    n_images = len(input_set)
+
+    #Training and validation sets
+    split_marker = int(n_images*(1 - validation_split))
+    train_set = input_set[:split_marker]
+    validation_set = input_set[split_marker:]
+    print("Training set: {t_size} validation set: {v_size}".format(t_size = len(train_set), v_size = len(validation_set)))
+
+    model.fit_generator(
+        _model_fit_data_feeder("training", source_loc, train_set, batch_size, input_labels, label_ids),
+        steps_per_epoch = int((len(train_set) + batch_size - 1)/batch_size),
+        epochs = n_epochs,
+        validation_data=_model_fit_data_feeder("validation", source_loc, validation_set, batch_size, input_labels, label_ids),
+        validation_steps=int((len(validation_set) + batch_size - 1)/batch_size))
--- train.py
+++ train.py
@@ -1,6 +1,6 @@
-"""Entry point to train the model.
+"""Trains a model and save it to the disk.
 
-    Usage:: python train.py <batch_size> <num_epochs> <[int] Size of training set. If not specified, process all.>
+    Usage:: python train.py <output_model_name> <batch_size> <num_epochs> <[int] Size of training set. If not specified, process all.>
 """
 #Basic imports
 from sys import argv, stdout
@@ -8,49 +8,44 @@ from sys import argv, stdout
 #Local imports
 from common import constants
 from models import model_1
-from model_utils import get_image_labels, get_label_ids, model_fit_data_feeder
+from model_utils import get_input_labels, get_label_ids, model_fit
 from utils import list_files
 
 if __name__ == "__main__":
     """Entry point to train the model.
 
-    Usage:: python train.py <batch_size> <num_epochs> <[int] Size of training set. If not specified, process all.>
+    Usage:: python train.py <output_model_name> <batch_size> <num_epochs> <[int] Size of training set. If not specified, process all.>
     """
     n_args = len(argv)
-    if n_args not in [3, 4]:
-        print("Syntax error. Usage:: python train.py <batch_size> <num_epochs> <[int] Size of training set. If not specified, process all.>") 
+    if n_args not in [4, 5]:
+        print("Syntax error. Usage:: python train.py <output_model_name> <batch_size> <num_epochs> <[int] Size of training set. If not specified, process all.>") 
         exit(-1)
     
-    
-    batch_size = int(argv[1])
-    n_epochs = int(argv[2])
-    n_images = int(argv[3]) if len(argv) == 4 else None
+    o_model = argv[1]
+    batch_size = int(argv[2])
+    n_epochs = int(argv[3])
+    n_images = int(argv[4]) if len(argv) == 5 else None
 
     dataset = "train"
     validation_split = 0.2
     input_shape = constants.IMG_SHAPE
     source_loc = constants.PROCESSED_DATASET_MAPPINGS[dataset]
     
-    image_labels = get_image_labels()
+    input_labels = get_input_labels()
     label_ids = get_label_ids()
     num_classes = len(label_ids)
-    img_files = list_files(source_loc, n_images)
-    n_images = len(img_files)
-    
-    #Training and validation sets
-    split_marker = int(n_images*(1 - validation_split))
-    train_set = img_files[:split_marker]
-    validation_set = img_files[split_marker:]
-    print("Training set: {t_size} validation set: {v_size}".format(t_size = len(train_set), v_size = len(validation_set)))
+    input_set = list_files(source_loc, n_images)
+
+    #Output model file
+    model_file = "{o_model}.h5".format(o_model = o_model)
 
     #Initialize the model
     model = model_1(input_shape, num_classes)
     model.summary()
 
     #Train the model
-    model.fit_generator(
-        model_fit_data_feeder("training", source_loc, train_set, batch_size, image_labels, label_ids),
-        steps_per_epoch = int((len(train_set) + batch_size - 1)/batch_size),
-        epochs = n_epochs,
-        validation_data=model_fit_data_feeder("validation", source_loc, validation_set, batch_size, image_labels, label_ids),
-        validation_steps=int((len(validation_set) + batch_size - 1)/batch_size))
+    model_fit(model, source_loc, input_set, input_labels, label_ids, batch_size, n_epochs, validation_split)
+
+    #Save model
+    print("Saving model {model_file}".format(model_file = model_file))
+    model.save(model_file, overwrite=True)
